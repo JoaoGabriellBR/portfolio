9beaec8a24eeb7c29c65fc6712a644e6d821e650
João Gabriel <joaoname9@gmail.com>
2023-09-12 12:48:18 -0300

    Revert "refactor: performance"
    This reverts commit ef9f7b1e6ac8f15dad7c4b7d1d5b924c0e625607.

--- src/components/ProjectItem/index.tsx
+++ src/components/ProjectItem/index.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { VscGithub } from "react-icons/vsc";
 import { GoLinkExternal } from "react-icons/go";
-import Link from "next/link";
 
 interface Project {
   name: string;
@@ -11,18 +10,17 @@ interface Project {
   site: string;
   repository: string;
   gradient: string;
-}
+};
 
 interface ProjectItemProps {
   project: Project;
 }
 
-const ProjectItem = ({ project }: ProjectItemProps) => {
+const ProjectItem = ( { project }: ProjectItemProps) => {
+
   const [isHovered, setIsHovered] = useState<boolean>(false);
 
-  const iconStyle = `${
-    isHovered ? "text-white" : "text-black"
-  } w-6 h-6 font-bold hover:opacity-80`;
+  const iconStyle = `${isHovered ? "text-white" : "text-black"} w-6 h-6 font-bold`;
   const handleChange = (value: boolean) => setIsHovered(value);
 
   return (
@@ -32,27 +30,19 @@ const ProjectItem = ({ project }: ProjectItemProps) => {
       className={`${
         isHovered
           ? "bg-transparent border-solid border-2 border-white"
-          : `bg-gradient-to-r ${project.gradient}`
+          : project.gradient
       } h-[22rem] p-5 rounded-lg flex flex-col items-center justify-between transition duration-500 ease-in-out`}
     >
       <div className="w-full flex flex-row justify-end items-center space-x-3">
-        <Link href={String(project.repository)}>
-          <button>
-            <VscGithub className={iconStyle} />
-          </button>
-        </Link>
-        <Link href={String(project.site)}>
-          <button>
-            <GoLinkExternal className={iconStyle} />
-          </button>
-        </Link>
+        <button>
+          <VscGithub className={iconStyle} />
+        </button>
+        <button>
+          <GoLinkExternal className={iconStyle} />
+        </button>
       </div>
 
-      <div
-        className={`${
-          isHovered ? "text-white" : "text-black"
-        } flex flex-col justify-between items-center space-y-10 text-center`}
-      >
+      <div className={`${isHovered ? 'text-white' : 'text-black'} flex flex-col justify-between items-center space-y-10 text-center`}>
         <h1 className="text-[2.5rem] font-black">{project.name}</h1>
         <p className="text-[0.6rem] font-bold tracking-[.15rem]">
           {project.technologies}
