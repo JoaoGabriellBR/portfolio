aa7ea5373f54d5a1145c05c2dcaa37a2f6613789
João Gabriel <joaoname9@gmail.com>
2023-09-13 14:29:42 -0300

    refactor: Form Types

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,49 +1,42 @@
 "use client"
-
 import { useState } from "react";
 import Input from "@/components/Input";
 import Button from "../Button";
 import { MdSend } from "react-icons/md";
 
 const Form = () => {
-
   const [email, setEmail] = useState<string>('');
   const [message, setMessage] = useState<string>('');
 
-  const handleChange = (e: any) => {
-    setEmail(e.target.value);
-  };
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  const handleChangeMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value);
 
   const inputValue = [
-    { label: "Seu Email", value: email, type: "email" },
-    { label: "Sua mensagem", value: message, type: "text" },
+    { id: "floating-input", label: "Seu Email", value: email, onChange: handleChangeEmail, type: "email" },
+    { id: "floating-textarea", label: "Sua mensagem", value: message, onChange: handleChangeMessage, type: "textarea" },
   ];
 
   return (
     <>
-      <section className="py-7 w-full space-y-[5rem] md:space-y-[20rem]">
+      <section className="py-20 w-full space-y-[5rem] md:space-y-[20rem]">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 flex flex-row justify-center items-center">
-
           <form className="w-6/12 space-y-10 flex flex-col justify-start items-start">
-
             <h1 className="text-[2rem]">Diga Olá! 👋</h1>
-
-            {inputValue?.map((input: any, index: number) => (
+            {inputValue?.map((input) => (
               <Input
-                key={index}
+                key={input.id}
+                id={input.id}
                 label={input.label}
                 value={input.value}
                 type={input.type}
-                onChange={handleChange} />
+                onChange={input.onChange}
+              />
             ))}
-
             <Button>
               <MdSend className="mr-2 w-[1.2rem] h-[1.2rem] font-bold" />
               Enviar mensagem
             </Button>
-
           </form>
-
         </div>
       </section>
     </>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,33 +1,51 @@
 "use client"
-
 import React, { useState } from 'react';
 
-const Input = ({ label, type, value, onChange }: any) => {
+interface InputProps {
+  id: string;
+  label: string;
+  value: string;
+  type: string;
+  onChange: (e: any) => void;
+}
 
-  const [isFocused, setIsFocused] = useState(false);
+const Input: React.FC<InputProps> = ({ id, label, value, type, onChange }) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
 
+  const inputStyles = "w-full py-3 px-2 bg-transparent border-b border-neutral-400 focus:outline-none focus:border-white";
+
   return (
     <div className="relative w-full">
       <label
-        className={`absolute top-0 left-2 transition-transform ${
-          isFocused || value ? '-translate-y-4 text-sm text-white' : 'translate-y-2 text-base text-neutral-400'
-        }`}
-        htmlFor="floating-input"
+        className={`absolute top-0 left-2 transition-transform ${isFocused || value ? '-translate-y-4 text-sm text-white' : 'translate-y-2 text-base text-neutral-400'
+          }`}
+        htmlFor={id}
       >
         {label}
       </label>
-      <input
-        id="floating-input"
-        type={type}
-        value={value}
-        onChange={onChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        className="w-full py-3 px-2 bg-transparent border-b border-neutral-400 focus:outline-none focus:border-white"
-      />
+      {type === 'textarea' ? (
+        <textarea
+          id={id}
+          value={value}
+          onChange={onChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          className={inputStyles}
+        />
+      ) : (
+        <input
+          id={id}
+          type={type}
+          value={value}
+          onChange={onChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          className={inputStyles}
+        />
+      )}
     </div>
   );
 };
