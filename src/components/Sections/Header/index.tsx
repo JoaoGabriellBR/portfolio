55298357d3861d18fb3ed05c7ce8f205c9145cb6
João Gabriel <joaoname9@gmail.com>
2023-09-14 14:14:16 -0300

    feat: Navigation

--- src/components/Sections/Header/index.tsx
+++ src/components/Sections/Header/index.tsx
@@ -1,12 +1,23 @@
+"use client"
+
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { HiMenuAlt1 } from "react-icons/hi";
 import logo from "@/assets/logo.png";
+import Navigation from "@/components/Navigation";
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+    const openMenu = () => setIsMenuOpen(true);
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
         <header className="pt-7 w-full">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 flex flex-row justify-between items-center">
+                
                 <Link href="/">
                     <Image
                         className="hover:opacity-80"
@@ -16,7 +27,24 @@ const Header = () => {
                         height={40}
                     />
                 </Link>
-                <HiMenuAlt1 className="cursor-pointer text-[2rem]" />
+
+                <div>
+                    <button onClick={openMenu}>
+                        <HiMenuAlt1 className="cursor-pointer text-[2rem] text-white" />
+                    </button>
+                    <Navigation isOpen={isMenuOpen} closeMenu={closeMenu} />
+                </div>
+
+                {/* <div>
+                    <button
+                        className="text-2xl focus:outline-none"
+                        onClick={toggleMenu}
+                    >
+                        &#9776;
+                    </button>
+                    <Navigation isOpen={isMenuOpen} onClose={toggleMenu} />
+                </div> */}
+
             </div>
         </header>
     )
