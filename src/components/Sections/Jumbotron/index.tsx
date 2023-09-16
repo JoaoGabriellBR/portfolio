7e2526e49121d3575b385bf4284e9fe9ada15566
João Gabriel <joaoname9@gmail.com>
2023-09-16 05:59:30 -0300

    refactor

--- src/components/Sections/Jumbotron/index.tsx
+++ src/components/Sections/Jumbotron/index.tsx
@@ -1,15 +1,21 @@
+import Button from "@/components/Button";
+import { FiDownload } from "react-icons/fi";
+
 const Jumbotron = () => {
-  
-  const titleStyle = "font-extrabold text-5xl md:text-6x1 lg:text-8xl ml-0 md:ml-[10rem]";
+  const titleStyle = "font-extrabold text-5xl md:text-6x1 lg:text-8xl";
 
   return (
     <section id="home" className="w-full">
-      <div className="max-w-6xl h-screen mx-auto px-4 sm:px-6 pb-[10rem] flex flex-col justify-center items-center md:items-start text-center md:text-start space-y-5">
-        <p className="text-neutral-400 font-black ml-0 md:ml-[10rem] tracking-[.15rem]">
+      <div className="max-w-6xl h-screen mx-auto px-4 sm:px-6 pb-[10rem] flex flex-col justify-center items-center text-center space-y-5">
+        <p className="text-neutral-400 font-black tracking-[.15rem]">
           JOÃO GABRIEL SILVA
         </p>
         <h1 className={titleStyle}>Desenvolvedor</h1>
         <h1 className={titleStyle}>Full Stack</h1>
+        <Button outline>
+          Currículo
+          <FiDownload className="ml-2 w-[1.2rem] h-[1.2rem] font-bold" />
+        </Button>
       </div>
     </section>
   );
--- src/components/Sections/Skills/index.tsx
+++ src/components/Sections/Skills/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { GrMysql } from "react-icons/gr";
 import { FaReact, FaNodeJs, FaSass } from "react-icons/fa";
 import { SiNextdotjs, SiTailwindcss, SiPrisma } from "react-icons/si";
@@ -8,8 +9,8 @@ import {
 } from "react-icons/bi";
 
 const Skills = () => {
-
-  const iconStyle =  "w-16 h-16 mb-4 hover:scale-125 transition duration-500 ease-in-out";
+  const iconStyle =
+    "w-16 h-16 mb-4 hover:scale-125 transition duration-500 ease-in-out";
 
   const technologies = [
     {
@@ -17,60 +18,70 @@ const Skills = () => {
       alt: "React.js Icon",
       color: "hover:text-react",
       icon: <FaReact className={iconStyle} />,
+      site: "https://legacy.reactjs.org",
     },
     {
       name: "Node",
       alt: "Node.js Icon",
       color: "hover:text-node",
       icon: <FaNodeJs className={iconStyle} />,
+      site: "https://nodejs.org",
     },
     {
       name: "Next",
       alt: "Next.js Icon",
       color: "hover:text-next",
       icon: <SiNextdotjs className={iconStyle} />,
+      site: "https://nextjs.org",
     },
     {
       name: "TypeScript",
       alt: "TypeScript Icon",
       color: "hover:text-typescript",
       icon: <BiLogoTypescript className={iconStyle} />,
+      site: "https://www.typescriptlang.org",
     },
     {
       name: "JavaScript",
       alt: "JavaScript Icon",
       color: "hover:text-javascript",
       icon: <BiLogoJavascript className={iconStyle} />,
+      site: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
     },
     {
       name: "Tailwind CSS",
       alt: "Tailwind CSS Icon",
       color: "hover:text-tailwind",
       icon: <SiTailwindcss className={iconStyle} />,
+      site: "https://tailwindcss.com",
     },
     {
       name: "Sass",
       alt: "Sass Icon",
       color: "hover:text-sass",
       icon: <FaSass className={iconStyle} />,
+      site: "https://sass-lang.com",
     },
     {
       name: "Prisma ORM",
       alt: "Prisma ORM Icon",
       color: "hover:text-prisma",
       icon: <SiPrisma className={iconStyle} />,
+      site: "https://sass-lang.com",
     },
     {
       name: "MySQL",
       alt: "MySQL Icon",
       color: "hover:text-mysql",
       icon: <GrMysql className={iconStyle} />,
+      site: "https://www.mysql.com",
     },
     {
       name: "PostgreSQL",
       alt: "PostgreSQL Icon",
       color: "hover:text-postgresql",
       icon: <BiLogoPostgresql className={iconStyle} />,
+      site: "https://www.postgresql.org",
     },
   ];
 
@@ -84,9 +95,11 @@ const Skills = () => {
               key={index}
               className="min-w-[12rem] flex flex-col justify-center items-center mb-10"
             >
-              <button className={`${tech.color}`} name={tech.alt}>
-                {tech.icon}
-              </button>
+              <Link href={tech.site} target="blank">
+                <button className={`${tech.color}`} name={tech.alt}>
+                  {tech.icon}
+                </button>
+              </Link>
               <p className="text-sm">{tech.name}</p>
             </div>
           ))}
