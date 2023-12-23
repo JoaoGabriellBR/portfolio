35835cd04182b8557f2ad2ba05d6b0a845c57756
João Gabriel <joaoname9@gmail.com>
2023-12-23 18:50:00 -0300

    feat: adding getFullYear Footer

--- src/components/Sections/Footer/index.tsx
+++ src/components/Sections/Footer/index.tsx
@@ -1,11 +1,14 @@
 import SocialMedias from "@/components/SocialMedias";
 
 const Footer = () => {
+
+    const year = new Date().getFullYear();
+
     return (
         <footer className="pb-7 w-full">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 flex flex-col justify-center items-center space-y-5">
                 <SocialMedias />
-                <p className="text-neutral-400">{" < © 2023 João Gabriel Silva /> "}</p>
+                <p className="text-neutral-400">{`< © ${year} João Gabriel Silva />`}</p>
             </div>
         </footer>
     )
