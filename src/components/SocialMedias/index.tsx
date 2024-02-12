46cd08e62b3ea86f2723b0febc28574786e202ad
João Gabriel <joaoname9@gmail.com>
2024-02-11 22:40:06 -0300

    fix: tooltip social medias

--- src/components/SocialMedias/index.tsx
+++ src/components/SocialMedias/index.tsx
@@ -1,25 +1,15 @@
 import Link from "next/link";
-import { Tooltip } from "react-tooltip";
 import { socialMedias, divIconsStyle, iconsStyle } from "@/utils/socialMedias";
 
 const SocialMedias = () => {
   return (
     <nav className="w-[10rem] flex flex-row justify-between items-center mt-5 md:mt-0">
       {socialMedias.map((social) => (
-        <>
-          <Link href={social.href} id={social.id} data-tooltip-delay-show={400}>
-            <div className={divIconsStyle}>
-              <social.icon className={iconsStyle} />
-            </div>
-          </Link>
-          <Tooltip
-            id="tooltip-anchor-show"
-            content={social.tooltip}
-            style={{ borderRadius: "1rem" }}
-            anchorSelect={`#${social.id}`}
-            place="top"
-          />
-        </>
+        <Link key={social.id} href={social.href} data-tooltip-delay-show={400}>
+          <div className={divIconsStyle}>
+            <social.icon className={iconsStyle} />
+          </div>
+        </Link>
       ))}
     </nav>
   );
